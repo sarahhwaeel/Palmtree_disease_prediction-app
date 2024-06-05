b2876cd9f03c17cc293748bdf2faf44f6aa0e352
sarahhwaeel <133380777+sarahhwaeel@users.noreply.github.com>
2024-06-06 02:35:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+import tensorflow as tf
 
-
-import cv2
-from os import listdir
-
-st.markdown(
-    """
-    <style>
-        .stButton>button {
-            display: inline !important;
-        }
-    </style>
-""",
-    unsafe_allow_html=True,
-)
+# Install OpenCV
+st.system("pip install opencv-python-headless")
 
 # Load the trained model
 @st.cache(allow_output_mutation=True)
@@ -29,19 +18,6 @@ def preprocess_image(img):
     img_array = np.array(img) / 255.0  # Normalize pixel values
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
-
-# Convert image to array
-def convert_image_to_array(image_dir):
-    try:
-        image = cv2.imread(image_dir)
-        if image is not None:
-            image = cv2.resize(image, (256, 256))
-            return image / 255.0  # Normalize pixel values
-        else:
-            return np.array([])
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
 # Main app
 def main():
@@ -67,16 +43,7 @@ def main():
         # Display prediction
         st.markdown(f"**<h3 style='font-size:24px'>Predicted Class: {predicted_class}</h3>**", unsafe_allow_html=True)
 
-        # Display pesticide suggestion
-        if predicted_class == 'brown spots':
-            pesticide_info = "Use Fungicidal sprays containing copper."
-        elif predicted_class == 'healthy':
-            pesticide_info = "No pesticide used."
-        elif predicted_class == 'white scale':
-            pesticide_info = "Use Chemical insecticides as buprofezin or pyriproxyfen."
-
-        st.markdown(f"**<h3 style='font-size:24px'>Pesticide suggested: {pesticide_info}</h3>**", unsafe_allow_html=True)
-
 if __name__ == "__main__":
     main()
+
 

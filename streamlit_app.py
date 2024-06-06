7770e974a362ff8abafa0982ea3071000f83dc2f
sarahhwaeel <133380777+sarahhwaeel@users.noreply.github.com>
2024-06-06 23:27:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
-
-
 import streamlit as st
 import numpy as np
 from PIL import Image
-
+import tensorflow as tf
 
 # Load the trained model
-@st.cache(allow_output_mutation=True)
+@st.cache_resource
 def load_model():
-    model_path = "/content/drive/My Drive/Data/Dataset/Date Palm data/palmtree_disease_model.h5"
+    model_path = "palmtree_disease_model.h5"  # Update this path as needed
     return tf.keras.models.load_model(model_path)
 
 # Preprocess the uploaded image
@@ -41,6 +39,16 @@ def main():
 
         # Display prediction
         st.markdown(f"**<h3 style='font-size:24px'>Predicted Class: {predicted_class}</h3>**", unsafe_allow_html=True)
+
+        # Display pesticide suggestion
+        if predicted_class == 'brown spots':
+            pesticide_info = "Use Fungicidal sprays containing copper."
+        elif predicted_class == 'healthy':
+            pesticide_info = "No pesticide used."
+        elif predicted_class == 'white scale':
+            pesticide_info = "Use Chemical insecticides as buprofezin or pyriproxyfen."
+
+        st.markdown(f"**<h3 style='font-size:24px'>Pesticide suggested: {pesticide_info}</h3>**", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()

ec1fb56ccadff30aaedf54f450495b20df68437f
sarahhwaeel <133380777+sarahhwaeel@users.noreply.github.com>
2024-06-12 22:50:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,31 +32,33 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Display uploaded image
-        img = Image.open(uploaded_file)
-        st.image(img, caption="Uploaded Image", width=300)
+        try:
+            # Display uploaded image
+            img = Image.open(uploaded_file)
+            st.image(img, caption="Uploaded Image", width=300)
 
-        # Preprocess the image
-        img_array = preprocess_image(img)
+            # Preprocess the image
+            img_array = preprocess_image(img)
 
-        # Make prediction
-        predictions = model.predict(img_array)
-        class_labels = ['brown spots', 'healthy', 'white scale']
-        predicted_class = class_labels[np.argmax(predictions)]
+            # Make prediction
+            predictions = model.predict(img_array)
+            class_labels = ['brown spots', 'healthy', 'white scale']
+            predicted_class = class_labels[np.argmax(predictions)]
 
-        # Display prediction
-        st.markdown(f"**<h3 style='font-size:24px'>Predicted Class: {predicted_class}</h3>**", unsafe_allow_html=True)
+            # Display prediction
+            st.markdown(f"**<h3 style='font-size:24px'>Predicted Class: {predicted_class}</h3>**", unsafe_allow_html=True)
 
-        # Display pesticide suggestion
-        if predicted_class == 'brown spots':
-            pesticide_info = "Use Fungicidal sprays containing copper."
-        elif predicted_class == 'healthy':
-            pesticide_info = "No pesticide used."
-        elif predicted_class == 'white scale':
-            pesticide_info = "Use Chemical insecticides as buprofezin or pyriproxyfen."
+            # Display pesticide suggestion
+            if predicted_class == 'brown spots':
+                pesticide_info = "Use Fungicidal sprays containing copper."
+            elif predicted_class == 'healthy':
+                pesticide_info = "No pesticide used."
+            elif predicted_class == 'white scale':
+                pesticide_info = "Use Chemical insecticides as buprofezin or pyriproxyfen."
 
-        st.markdown(f"**<h3 style='font-size:24px'>Pesticide suggested: {pesticide_info}</h3>**", unsafe_allow_html=True)
+            st.markdown(f"**<h3 style='font-size:24px'>Pesticide suggested: {pesticide_info}</h3>**", unsafe_allow_html=True)
+        except Exception as e:
+            st.error(f"Error processing image: {e}")
 
 if __name__ == "__main__":
     main()
-
